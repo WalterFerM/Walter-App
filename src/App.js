9fcd45c132d889cb908ee890c73c148b475e7aa3
WalterFerM <wffmen@gmail.com>
2022-04-26 01:26:02 -0500

    fix-bug5.1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
-// import Card from './components/Card.jsx';
+import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
-
+import { Cairns } from './data';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import SearchBar from './components/SearchBar.jsx';
 import Navbar from './components/Navbar';
+
 
 const API_KEY = "b9fd7b8644a72c87e5bd3b214a46a743";
 
@@ -57,8 +58,6 @@ function App() {
       });
   }
 
-
-
   return (
     <div className="App">
       <Header />
@@ -69,13 +68,13 @@ function App() {
         />
       </div>
       <div>
-        {/* <Card
+        <Card
           max={Cairns.main.temp_max}
           min={Cairns.main.temp_min}
           name={Cairns.name}
           img={Cairns.weather[0].icon}
-          onClose={() => alert(Cairns.name)}
-        /> */}
+          onClose={handleRemoveCity}
+        />
         <Cards
           cities={cities} onRemove={handleRemoveCity}
         />
